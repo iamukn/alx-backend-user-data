8624ca3bcd3989b60da8fe2d8263cd6b677709f5
iamukn <n.u.kingsley@gmail.com>
2023-08-03 21:10:50 +0000

    explicitly specified the encoding

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,17 +2,17 @@
 """ Script that validates a password (UKN)"""
 
 
-from bcrypt import checkpw, gensalt, hashpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """ Receives a string argument and returns
     a salted, hashed password which is a byte
     """
-    return hashpw(password.encode('utf-8'), gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Method that validates a password
     """
-    return checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
