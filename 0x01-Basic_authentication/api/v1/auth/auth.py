1f62ec6a846e15c74b330aa646711fc2a2fe4816
iamukn <n.u.kingsley@gmail.com>
2023-08-09 20:14:54 +0000

    route auth definition

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,22 @@ class Auth:
     """authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ public method that returns a boolean """
-        return False
+        if path is None or excluded_paths is [] or excluded_paths is None:
+            return True
+        if path not in excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """pubic method that returns a string"""
