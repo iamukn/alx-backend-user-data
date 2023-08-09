ef6e04db5994a01e5a3ea00716756afed4064e72
iamukn <n.u.kingsley@gmail.com>
2023-08-09 20:20:32 +0000

    hot fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,10 +9,12 @@ class Auth:
     """authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ public method that returns a boolean """
-        if path is None or excluded_paths is [] or excluded_paths is None:
+        if path is None:
             return True
-        if path not in excluded_paths:
+
+        if excluded_paths is None or excluded_paths == []:
             return True
+
         if path in excluded_paths:
             return False
 
@@ -24,6 +26,7 @@ class Auth:
             elif excluded_path[-1] == "*":
                 if path.startswith(excluded_path[:-1]):
                     return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
