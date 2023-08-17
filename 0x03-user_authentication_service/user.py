87b9769d654e32e0db685a7eebf4ae0b48bff488
iamukn <n.u.kingsley@gmail.com>
2023-08-17 23:00:22 +0000

    Creating a table using sqlalchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" The User Model """
+
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine("sqlite:///:memory:", echo=False)
+Base = declarative_base()
+
+
+class User(Base):
+    """Class user that creates a table
+    in the database
+    """
+
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+
+Base.metadata.create_all(engine)
