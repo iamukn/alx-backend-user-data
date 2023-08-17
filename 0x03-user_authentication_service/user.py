5ac0b073cdfa4698e1c533765e461037bfdad12a
iamukn <n.u.kingsley@gmail.com>
2023-08-17 23:04:19 +0000

    hot fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,7 +14,7 @@ class User(Base):
     in the database
     """
 
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
